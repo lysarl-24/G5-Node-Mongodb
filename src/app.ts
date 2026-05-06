259e0c78c513c9b30c4fcf75966f40bfeb16f4cb
LY.SARL <ley928445@gmail.com>
2026-05-06 20:20:11 +0700

    Connect Database

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,17 @@
 import express from 'express';
-import env from 'dotenv';
-env.config();
+import dotenv from 'dotenv';
+import connectDB from './config/db.ts';
+
+dotenv.config();
+
+// connect database
+connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST || 'localhost';
+
+app.use(express.json());
 
 app.listen(PORT, HOST, () => {
     console.log(`Server running on http://${HOST}:${PORT}`);
